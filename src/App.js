fcff0cbe5009359c1363e2f5ac185470895953a9
Hadhazy Gabor <gaborhadhazy07@gmail.com>
2022-03-28 13:54:40 +0200

    json test

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from "react"
 import "./styles/main.scss"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
-import IntroductionSection from "./Components/IntroductionSection"
 import Content from "./Components/Content"
 
 function App() {
   return (
-      <div className="App">
-           <Header />
-           <IntroductionSection />
-          <Content />
-            <Footer />
-      </div>
-
-
+    <div className='App'>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   )
 }
 export default App
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,83 +1,95 @@
-import React, {useState, useEffect} from "react"
-import { Typography, Button, Container, Grid, CssBaseline, AppBar, Toolbar, TextField, InputBase, Menu, MenuItem, ButtonGroup } from "@material-ui/core"
-import useStyles from "./styles"
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from "@material-ui/icons/Search"
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import React, { useState, useEffect } from "react"
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown"
 import axios from "axios"
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
-import SettingsIcon from '@material-ui/icons/Settings'
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import {createTheme, ThemeProvider} from "@material-ui/core/styles";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom"
 
+function HeaderMenuItem({ item_data }) {
+  return (
+    <div className='menu-item'>
+      {item_data.title.toUpperCase()}
 
-function HeaderMenuItem({item_data}) {
+      {item_data.items && <KeyboardArrowDownIcon />}
 
-    return(
-        <p className="menu-item">
-        {item_data.title.toUpperCase()}
-
-        {
-            item_data.items && <KeyboardArrowDownIcon />
-        }
-
-        {
-            item_data.items && <div className="popup-menu">
-                {
-                    item_data.items.map(i => <a key={i} className="menu-item">{i}</a>)
-                }
-            </div>
-        }
-
-
-    </p>
-    )
+      {item_data.items && (
+        <div className='popup-menu'>
+          {item_data.items.map((i) => (
+            <a key={i} className='popup-items'>
+              {i}
+            </a>
+          ))}
+        </div>
+      )}
+      
+    </div> 
+  )
 }
 
 function HeaderMenu() {
-    const [menu_list, set_menu_list] = useState([])
+  const [menu_list, set_menu_list] = useState([])
 
-    useEffect(() => {
-        axios({
-            method: "get",
-            url: `${process.env.REACT_APP_API_URL}/categories`
-        }).then(res => set_menu_list(res.data))
-    }, [])
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}/categories`,
+    }).then((res) => set_menu_list(res.data))
+  }, [])
+
+  return (
+<div className='menu'>
+
+  {menu_list.map((item_data) => {
+         return <HeaderMenuItem key={item_data.id} item_data={item_data} />
+        })}
+
+</div>
+  )
+}
+
+function Settings() {
+    const [theme, setTheme] = useState("Light")
+
+    const handleThemeSwitcher = () => {
+        if(theme === "Light") {
+            setTheme("Dark")
+        } else {
+            setTheme("Light")
+        }
+    }
 
     return (
-        <div className="menu">
-            {
-                menu_list.map(item_data => <HeaderMenuItem key={item_data.id} item_data={item_data} />)
-            }
-        </div>
+        <div className="icon">
+            <button onClick={handleThemeSwitcher} className="button">{theme} theme</button>
+        </div> 
     )
 }
 
 
 function Header() {
-    const  [site_info, set_site_info] = useState([])
+  const [site_info, set_site_info] = useState([])
 
-    const fetch_site_info = () => {
-        axios({
-            method: "get",
-            url: `${process.env.REACT_APP_API_URL}/site_info`
-        }).then(res => set_site_info(res.data))
-    }
+  const fetch_site_info = () => {
+    axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}/site_info`,
+    }).then((res) => set_site_info(res.data))
+  }
 
-    useEffect(() => {
-        fetch_site_info();
-    }, [])
-
-
+  useEffect(() => {
+    fetch_site_info()
+  }, [])
 
   return (
-      <div className="header-container">
-          <div className="content-container">
-              <HeaderMenu />
-          </div>
-      </div>
+<div className='header-container'>
+        <HeaderMenu />
+  <div className="button-wrapper">
+    <button className="button">Sign up</button>
+    <button className="button">Create Account</button>
+  </div>
+
+  <div className="settings-section">
+    <Settings />
+  </div>   
+</div>
   )
 }
 
