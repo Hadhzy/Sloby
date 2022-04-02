5efd6a96d0c054b55690490f4d481e16270b47f6
Hadházy Tamás <helsgithub15@gmail.com>
2022-04-02 18:45:08 +0200

    Create Links and routers with react-router-dom

---
+++ src/Components/SubComponents/Settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Settings(props) {
+    return (
+        <div className="content-base">
+            Settings
+        </div>
+    );
+}
+
+export default Settings;
