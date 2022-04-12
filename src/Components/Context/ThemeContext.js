649c39c2802b283471be1e401524ce42e97c7a8b
Hadhazy Gabor <gaborhadhazy07@gmail.com>
2022-04-12 17:07:47 +0200

    ThemeContext file add.

---
+++ src/Components/Context/ThemeContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react"
+import useLocalStorage from "use-local-storage"
+
+export const ThemeContext = createContext(true)
+
+export const ThemeContextProvider = (props) => {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light", "")
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light"
+    setTheme(newTheme)
+  }
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme: theme,
+        switchTheme: switchTheme,
+      }}
+    >
+      {props.children}
+    </ThemeContext.Provider>
+  )
+}
