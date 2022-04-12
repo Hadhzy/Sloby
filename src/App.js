f0a3b4ca3f40a79653180e8a7c658d9e92c5289b
Hadhazy Gabor <gaborhadhazy07@gmail.com>
2022-04-12 17:04:32 +0200

    Extended the settings Component, theme change

--- src/App.js
+++ src/App.js
@@ -10,30 +10,38 @@ import OurProject from "./Components/SubComponents/OurProject/OurProject"
 import Settings from "./Components/SubComponents/Settings/Settings"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import User from "./Components/SubComponents/User"
-import {SettingsDataContext, SettingsDataProvider} from "./Components/Context/SettingsContext";
+import {
+  SettingsDataContext,
+  SettingsDataProvider,
+} from "./Components/Context/SettingsContext"
+import {
+  ThemeContext,
+  ThemeContextProvider,
+} from "./Components/Context/ThemeContext"
 
 function App() {
   return (
     <BrowserRouter>
-      <SettingsDataProvider>
-        <div className='App'>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Content />} />
-            <Route path='/user' element={<User />} />
-            <Route path='/categories/:category/:subcategory' />
+      <ThemeContextProvider>
+        <SettingsDataProvider>
+          <div className='App'>
+            <Header />
+            <Routes>
+              <Route path='/' element={<Content />} />
+              <Route path='/user' element={<User />} />
+              <Route path='/categories/:category/:subcategory' />
 
-            <Route path='categories/help/*' element={<Help />} />
-            <Route path='categories/about-us/*' element={<AboutUs />} />
-            <Route path='categories/docs/*' element={<Docs/>} />
-            <Route path='categories/our-project/*' element={<OurProject />} />
-            <Route path='settings' element={<Settings/>} />
-          </Routes>
+              <Route path='categories/help/*' element={<Help />} />
+              <Route path='categories/about-us/*' element={<AboutUs />} />
+              <Route path='categories/docs/*' element={<Docs />} />
+              <Route path='categories/our-project/*' element={<OurProject />} />
+              <Route path='settings' element={<Settings />} />
+            </Routes>
 
-          <Footer />
-        </div>
-      </SettingsDataProvider>
-
+            <Footer />
+          </div>
+        </SettingsDataProvider>
+      </ThemeContextProvider>
     </BrowserRouter>
   )
 }
--- src/Components/SubComponents/Settings/Settings.js
+++ src/Components/SubComponents/Settings/Settings.js
@@ -1,95 +1,101 @@
-import React, {useContext, useState} from 'react';
-import {SettingsDataContext} from "../../Context/SettingsContext";
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React, { useContext, useState } from "react"
+import { SettingsDataContext } from "../../Context/SettingsContext"
+import { styled } from "@mui/material/styles"
+import Card from "@mui/material/Card"
+import CardHeader from "@mui/material/CardHeader"
+import CardMedia from "@mui/material/CardMedia"
+import CardContent from "@mui/material/CardContent"
+import CardActions from "@mui/material/CardActions"
+import Collapse from "@mui/material/Collapse"
+import Avatar from "@mui/material/Avatar"
+import IconButton from "@mui/material/IconButton"
+import Typography from "@mui/material/Typography"
+import { red } from "@mui/material/colors"
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import ShareIcon from "@material-ui/icons/Share"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import { ThemeContext } from "../../Context/ThemeContext"
 
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
+  const { expand, ...other } = props
+  return <IconButton {...other} />
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
-}));
-
+}))
 
 function Settings(props) {
-    const [expanded, setExpanded] = useState(false)
+  const [expanded, setExpanded] = useState(false)
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded)
-    }
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
 
-    const {settings} = useContext(SettingsDataContext)
-    return (
-        <div className="settings-container">
-            <div className="title">
-                Settings
+  const { settings } = useContext(SettingsDataContext)
+  const { theme, switchTheme } = useContext(ThemeContext)
+  return (
+    <div className='settings-container' data_theme={theme}>
+      <div className='title'>Settings</div>
+      <div className='underline'></div>
+      <div className='options'>
+        <div className='settings-item'>
+          {settings.map((setting) => (
+            <div>
+              <Card className='setting-item' sx={{ width: 1000 }}>
+                <CardHeader
+                  className='card-header'
+                  action={
+                    <IconButton aria-label='settings'>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title={setting.title}
+                  subheader={setting.subheader}
+                />
+                <CardContent>
+                  <Typography>
+                    {setting.description}
+                    <button onClick={switchTheme}>
+                      click to change the theme
+                    </button>
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  {/*<IconButton aria-label="add to favorites">*/}
+                  {/*    <FavoriteIcon/>*/}
+                  {/*</IconButton>*/}
+                  {/*<IconButton aria-lebel="share">*/}
+                  {/*    <ShareIcon/>*/}
+                  {/*</IconButton>*/}
+                  Our Icons system
+                  <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label='show more'
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout='auto' unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph>{setting.content_title}</Typography>
+                    <Typography paragraph>
+                      {setting.long_description}
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+              </Card>
             </div>
-            <div className="underline"></div>
-            <div className="options">
-                <div className="settings-item">
-                    {settings.map(setting => <div>
-                        <Card className="setting-item" sx={{width: 1000}}>
-                            <CardHeader  className="card-header" action={
-                                <IconButton aria-label="settings">
-                                     <MoreVertIcon/>
-                                </IconButton>
-                            } title={setting.title} subheader={setting.subheader}
-                            />
-                           <CardContent>
-                               <Typography>
-                                   {setting.description}
-                               </Typography>
-                           </CardContent>
-                            <CardActions disableSpacing>
-                                {/*<IconButton aria-label="add to favorites">*/}
-                                {/*    <FavoriteIcon/>*/}
-                                {/*</IconButton>*/}
-                                {/*<IconButton aria-lebel="share">*/}
-                                {/*    <ShareIcon/>*/}
-                                {/*</IconButton>*/}
-                                Our Icons system
-                                <ExpandMore
-                                expand={expanded}
-                                onClick={handleExpandClick}
-                                aria-expanded={expanded}
-                                aria-label="show more">
-                                    <ExpandMoreIcon/>
-                                </ExpandMore>
-                            </CardActions>
-                            <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{setting.content_title}</Typography>
-          <Typography paragraph>
-              {setting.long_description}
-          </Typography>
-        </CardContent>
-      </Collapse>
-
-                        </Card>
-
-                    </div>
-
-                    )}</div>
-            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default Settings;
+export default Settings
