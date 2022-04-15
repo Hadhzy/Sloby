f49ce1cbcadacbe589ec023ed30e527212cd69b9
Hadházy Tamás <helsgithub15@gmail.com>
2022-04-15 17:30:06 +0200

    Create a route system for menus item

--- src/Components/SubComponents/Docs/Docs.js
+++ src/Components/SubComponents/Docs/Docs.js
@@ -6,6 +6,8 @@ function Docs(props) {
         <div className="content-base">
             <Routes>
                 <Route path="/" element={<p>Docs</p>}/>
+                <Route path="download" element={<p>Download</p>}/>
+                <Route path="see-right-now" element={<p>See right now</p>}/>
             </Routes>
         </div>
     );
