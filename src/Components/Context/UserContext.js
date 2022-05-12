456fcd9b312c688d2da4dba18e9bac219a79413e
Hadházy Tamás <helsgithub15@gmail.com>
2022-05-12 16:12:54 +0200

    Cookie and token things

--- src/Components/Context/UserContext.js
+++ src/Components/Context/UserContext.js
@@ -1,11 +1,24 @@
 import React, { createContext, useState, useEffect } from "react"
 import axios from "axios"
+import {useCookies} from "react-cookie";
 
 export const UserContext = createContext(true)
 
 export const UserContextProvider = (props) => {
   const [logged_in, set_logged_in] = useState(false)
   const [token, set_token] = useState(null)
+  const [cookies, setCookie, removeCookie] = useCookies()
+
+  const DAYS_TO_SECONDS = 86400
+
+    const secondsCalculator = (days) => {
+        return days *  DAYS_TO_SECONDS
+    }
+
+  const _store_token = (value) => {
+    set_token(value)
+    setCookie("token", value, {path: "/", sameSite:"strict", maxAge: secondsCalculator(1)})
+  }
 
   const log_in_user = async (email, password) => {
     try {
@@ -13,11 +26,13 @@ export const UserContextProvider = (props) => {
       const config = { headers: {"content-type": "application/json"} }
 
       let response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login/`, user_data, config)
-      console.log(response)
+      _store_token(response.data.key)
     } catch (error) {
       console.log(error)
     }
   }
+
+
 
   return (
     <UserContext.Provider
