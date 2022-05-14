926fa1206457e6d7c611d602873c3585525efe7d
Hadházy Tamás <helsgithub15@gmail.com>
2022-05-14 10:21:21 +0200

    Base user authentication

--- src/Components/Context/UserContext.js
+++ src/Components/Context/UserContext.js
@@ -14,10 +14,23 @@ export const UserContextProvider = (props) => {
     const secondsCalculator = (days) => {
         return days *  DAYS_TO_SECONDS
     }
+  const check_token = () => {
+    if(cookies.token){
+      set_token(cookies.token)
+      set_logged_in(true)
+    }
+  }
 
   const _store_token = (value) => {
     set_token(value)
     setCookie("token", value, {path: "/", sameSite:"strict", maxAge: secondsCalculator(1)})
+    set_logged_in(true)
+  }
+
+  const _remove_token = (value) => {
+    removeCookie("token", {path: "/"})
+    set_token(null)
+    set_logged_in(false)
   }
 
   const log_in_user = async (email, password) => {
@@ -34,24 +47,28 @@ export const UserContextProvider = (props) => {
 
   const log_out_user = async () => {
     try  {
+      console.log("hi")
       const config = {
         headers: { authorization: `token ${token}` }
       }
 
-       await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/logout/`, config)
+       await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/logout/`, config).then(res => _remove_token())
 
     } catch (error) {
       console.log(error) 
     }
   }
 
-
+  useEffect( () => {
+    check_token()
+  }, [])
 
   return (
     <UserContext.Provider
       value={{
         log_in_user: log_in_user,
-        log_out_user: log_out_user
+        log_out_user: log_out_user,
+        logged_in: logged_in
       }}
     >
       {props.children}
