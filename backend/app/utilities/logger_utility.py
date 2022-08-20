759bf18ed204a1217476af51aac06b2dc1837fab
Hadházy Tamás <helsgithub15@gmail.com>
2022-08-20 10:08:16 +0200

    Create logger utility

--- backend/app/utilities/logger_utility.py
+++ backend/app/utilities/logger_utility.py
@@ -2,5 +2,30 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def setup_logger():
-    print("This is just a simple logger function, because we need to debug code, we are not superheros")
+class Logger:
+
+    def __init__(self):
+        self.get_logger()
+
+    # noinspection PyMethodMayBeStatic
+    def setup_logger(self):
+        self.logger = logging.getLogger("sloby")
+        self.logger.setLevel(logging.DEBUG)
+
+    # noinspection PyMethodMayBeStatic
+    def create_file(self):
+        log_file = "sloby.log"
+        ch = RotatingFileHandler(log_file, mode="a", maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=False)
+        # set the level
+        ch.setLevel(logging.DEBUG)
+        # create a formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # add formatter to ch
+        ch.setFormatter(formatter)
+        # add ch to logger
+        self.logger.addHandler(ch)
+
+    def get_logger(self):
+        self.setup_logger()
+        self.create_file()
+        return self.logger
