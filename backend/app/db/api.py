648d1da21ac59930133dc089f2aa23ed19543d7d
Hadházy Tamás <helsgithub15@gmail.com>
2022-08-22 10:46:15 +0200

    Extend the requirements txt file with psycopg, and create a config file

--- backend/app/db/api.py
+++ backend/app/db/api.py
@@ -1,3 +1,13 @@
+# standard library imports
+import logging
+import random
+from uuid import uuid4
+
+# third party imports
+from psycopg import Connection, connect, errors
+from psycopg.rows import class_row
+
+# this project
 
 class SlobyDB:
     pass
