89983d06a8d4e5390ebed8d190045ab32aed831b
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-09-10 07:05:52 +0200

    barely done with the sidebar carry on the content and funtionality

--- src/Components/Editor/ProjectsSection/SideMenuBar.jsx
+++ src/Components/Editor/ProjectsSection/SideMenuBar.jsx
@@ -4,9 +4,11 @@ import {Link} from "react-router-dom"
 import { BiHomeAlt, BiNews } from 'react-icons/bi'
 import { AiFillHeart } from "react-icons/ai"
 import { SiProgress } from "react-icons/si"
+import { HiOutlineUserCircle } from "react-icons/hi"
+import {AiOutlineFundProjectionScreen} from "react-icons/ai";
 
 const data = [
-    {id: 0, title: "Editor", icon: <BiHomeAlt />, path: "/editor/dashboard"},
+    {id: 0, title: "Projects", icon: <AiOutlineFundProjectionScreen />, path: "/editor/dashboard"},
     {id: 1, title: "Home Page", icon: <BiHomeAlt />, path: "/"},
     {id: 2, title: "Favorite Projects", icon: <AiFillHeart />, path: "/editor/dashboard/favorite-projects"},
     {id: 3, title: "News", icon: <BiNews />, path: "/editor/dashboard/news"},
@@ -20,23 +22,40 @@ const SideMenuBar = () => {
         setActiveMenu(item)
     }
 
+    const UserDataTag = () => {
+        return (
+            <div className="user-data-tag-container">
+               <div className="user-content">
+                     <div className="user-avatar">
+                        <img src="https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80" alt="" className="user-icon"/>
+                    </div>
+                    <div className="user-name">
+                        gaborhadhazy
+                    </div>
+               </div>
+            </div>
+        )
+    }
+
     return(
         <div className='menu-bar-container'>
-            {data.map(menu_bar_item => {
-                return (
-                    <Link 
-                    onClick={() => handleMenuItem(menu_bar_item.title)}
-                    to={menu_bar_item.path} 
-                    key={menu_bar_item.id} 
-                    className={`off-link-dec menu-bar-item ${activeMenu === menu_bar_item.title ? "menu-bar-item-active" : ""} `} 
-                    >
-                        <div className={`${activeMenu === menu_bar_item.title ? "menu-item-icon-active" : "menu-item-icon"}`}>{menu_bar_item.icon}</div>
-                        <div className={`${activeMenu === menu_bar_item.title ? "menu-bar-title-active" : "menu-bar-title"}`}>
-                            {menu_bar_item.title}
-                        </div>
-                    </Link>
-                )
-            })}
+            <UserDataTag />
+            <div className="project-underline"></div>
+                {data.map(menu_bar_item => {
+                    return (
+                        <Link 
+                        onClick={() => handleMenuItem(menu_bar_item.title)}
+                        to={menu_bar_item.path} 
+                        key={menu_bar_item.id} 
+                        className={`off-link-dec menu-bar-item ${activeMenu === menu_bar_item.title ? "menu-bar-item-active" : ""} `} 
+                        >
+                            <div className={`${activeMenu === menu_bar_item.title ? "menu-item-icon-active" : "menu-item-icon"}`}>{menu_bar_item.icon}</div>
+                            <div className={`${activeMenu === menu_bar_item.title ? "menu-bar-title-active" : "menu-bar-title"}`}>
+                                {menu_bar_item.title}
+                            </div>
+                        </Link>
+                    )
+                })}
         </div>
     )
 }
