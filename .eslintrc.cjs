09a932453b203cf48212ffd80afe664ca38940ef
hikmet <aydin96tr@gmail.com>
2022-12-09 21:17:18 +0300

    config preparation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,12 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'react-app',
+    'react-app/jest',
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
   ],
   overrides: [],
@@ -18,13 +22,12 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    semi: 0,
+    semi: 1,
     'comma-dangle': 0,
     // 'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'prettier/prettier': [0, { endOfLine: 'auto' }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 }
