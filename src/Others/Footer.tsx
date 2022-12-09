7c766387690aa0e0bfcc7be896a5a0848aae803e
hikmet <aydin96tr@gmail.com>
2022-12-10 01:49:30 +0300

    edited

--- src/Others/Footer.tsx
+++ src/Others/Footer.tsx
@@ -14,41 +14,54 @@ export function Footer() {
   //   currentUrl: currentUrl.pathname,
   // });
 
-  return (
-    <div className="theme-case-footer">
-      <div className="footer-container">
-        {footer ? (
-          <div>
-            {footer.map((footer_item: IFooterItem) => (
-              <div className="footer-items content-base" key={footer_item.id}>
-                <div className="footer-text footer-title">{footer_item.title}</div>
-                <div>
-                  {footer_item.items.map((item, index) => {
-                    return (
-                      <div className="footer-text footer-links" key={index}>
-                        {item}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="footer-text">{footer_item.news_letter_description}</div>
-                {footer_item.input && footer_item.icons && <OptionalInput footer_item={footer_item} />}
-
-                <div className="footer-bellow">
-                  {footer_item.optional &&
-                    footer_item.optional.map((item) => {
-                      return (
-                        <div className="footer-bellow-content" key={item}>
-                          {item}
-                        </div>
-                      );
-                    })}
-                </div>
-              </div>
-            ))}
+  if (footer) {
+    const footerItems = footer.map((footer_item: IFooterItem) => {
+      const footerLinks = footer_item.items.map((item, index) => {
+        return (
+          <div className="footer-text footer-links" key={index}>
+            {item}
           </div>
-        ) : null}
+        );
+      });
+
+      const optionalLinks = footer_item.optional.map((item) => {
+        return (
+          <div className="footer-bellow-content" key={item}>
+            {item}
+          </div>
+        );
+      });
+
+      if (footer_item.input && footer_item.icons) {
+        return (
+          <div className="footer-items content-base" key={footer_item.id}>
+            <div className="footer-text footer-title">{footer_item.title}</div>
+            <div>{footerLinks}</div>
+            <div className="footer-text">{footer_item.news_letter_description}</div>
+            <OptionalInput footer_item={footer_item} />
+            <div className="footer-bellow">{footer_item.optional && optionalLinks}</div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="footer-items content-base" key={footer_item.id}>
+            <div className="footer-text footer-title">{footer_item.title}</div>
+            <div>{footerLinks}</div>
+            <div className="footer-text">{footer_item.news_letter_description}</div>
+            <div className="footer-bellow">{footer_item.optional && optionalLinks}</div>
+          </div>
+        );
+      }
+    });
+
+    return (
+      <div className="theme-case-footer">
+        <div className="footer-container">
+          <div>{footerItems}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
