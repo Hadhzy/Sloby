8fb7bb905e44b0e32d2a50623fe50eb6a293f55f
Coddo-Python <coddobusiness@gmail.com>
2022-12-24 12:13:17 +0400

    Add signup page and input validation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        'gradientMove': 'gradient_move 8s ease-in-out infinite',
-      },
-      keyframes: {
-        gradient_move: {
-          '0%, 100%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                'gradientMove': 'gradient_move 8s ease-in-out infinite',
+                'shake': 'shake 4.72s ease',
+            },
+            keyframes: {
+                gradient_move: {
+                    '0%, 100%': {'background-position': '0% 50%'},
+                    '50%': {'background-position': '100% 50%'},
+                },
+                shake: {
+                    '0%': {transform: 'translate(0,0)'},
+                    '1.78571%': {transform: 'translate(10px,0)'},
+                    '3.57143%': {transform: 'translate(0,0)'},
+                    '5.35714%': {transform: 'translate(10px,0)'},
+                    '7.14286%': {transform: 'translate(0,0)'},
+                    '8.92857%': {transform: 'translate(10px,0)'},
+                    '10.71429%': {transform: 'translate(0,0)'},
+                    '100%': {transform: 'translate(0,0)'}
+                }
+            },
+            colors: {
+                'back': '#11162b',
+                'dark-dark': '#28293d',
+                'dark-mid': '#555870',
+                'dark-light': '#9090a7',
+                'red-dark': '#ff3b3b',
+                'red-mid': '#ff5c5c',
+                'red-light': '#ff8080',
+                'green-dark': '#05c270',
+                'green-mid': '#39da8a',
+                'green-light': '#57eba3',
+                'blue-dark': '#0063f8',
+                'blue-mid': '#5b8dee',
+                'blue-light': '#9dbff8',
+                'yellow-dark': '#ffcc00',
+                'yellow-mid': '#fedd49',
+                'yellow-light': '#feed73'
+            }
+        },
+        backgroundSize: {
+            'auto': 'auto',
+            'cover': 'cover',
+            'contain': 'contain',
+            'enlarge': '190%',
         }
-      },
-      colors: {
-        'back': '#11162b',
-        'dark-dark': '#28293d',
-        'dark-mid': '#555870',
-        'dark-light': '#9090a7',
-        'red-dark': '#ff3b3b',
-        'red-mid': '#ff5c5c',
-        'red-light': '#ff8080',
-        'green-dark': '#05c270',
-        'green-mid': '#39da8a',
-        'green-light': '#57eba3',
-        'blue-dark': '#0063f8',
-        'blue-mid': '#5b8dee',
-        'blue-light': '#9dbff8',
-        'yellow-dark': '#ffcc00',
-        'yellow-mid': '#fedd49',
-        'yellow-light': '#feed73'
-      }
     },
-    backgroundSize: {
-      'auto': 'auto',
-      'cover': 'cover',
-      'contain': 'contain',
-      'enlarge': '190%',
-    }
-  },
-  plugins: [require('@tailwindcss/forms'),],
+    plugins: [require('@tailwindcss/forms'),],
 }
