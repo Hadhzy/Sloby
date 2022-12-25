689a92833274c023f24e48fc27563f406791bc8a
Coddo-Python <coddobusiness@gmail.com>
2022-12-25 20:03:29 +0400

    Allow sign in with Google

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -7,3 +7,15 @@ export async function loggedIn(supabase: any, router: any, redirect: string) {
         await router.push(redirect);
     }
 }
+
+export const getURL = () => {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        'http://localhost:3000/editor/dashboard';
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`;
+    // Make sure to including trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+    return url;
+};
--- pages/editor/dashboard.tsx
+++ pages/editor/dashboard.tsx
@@ -1,5 +1,27 @@
+import {useSupabaseClient} from "@supabase/auth-helpers-react";
+import {useEffect, useState} from "react";
+
 export default function Dashboard() {
+    const supabase = useSupabaseClient();
+    const [loading, setLoading] = useState(true);
+    const [username, setUsername] = useState('');
+
+    async function getSupabaseData() {
+        const {
+            data: {session},
+        } = await supabase.auth.getSession()
+
+        setUsername(session?.user?.user_metadata.username);
+    }
+
+    useEffect(() => {
+        setLoading(true);
+        getSupabaseData().then(() => {
+            setLoading(false);
+        });
+    }, []);
+
     return (
-        <p>hello there</p>
+        <p>hello there {loading ? 'Loading ...' : username}</p>
     )
 }
