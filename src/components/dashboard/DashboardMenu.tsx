024c5a8dc8ac06af126d535a534ea88901b5661c
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-12-27 09:35:24 +0100

    Added basic dashboard routing

--- src/components/dashboard/DashboardMenu.tsx
+++ src/components/dashboard/DashboardMenu.tsx
@@ -5,11 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBarsProgress} from '@fortawesome/free-solid-svg-icons'
 import { faGear } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
 
 export default function DashboardMenu() {
   return <div className='bg-[#111111] h-screen w-[90px]  border-[#202020] border-r-2 flex flex-col justify-between'>
     <div className='flex flex-col items-center'>
-      <Image alt="Sloby Logo" className='mt-[10px]' src={"/images/Sloby Logo Dark.svg"} width={70} height={70}/>
+      <Link href='/editor/dashboard'>
+        <Image alt="Sloby Logo" className='mt-[10px]' src={"/images/Sloby Logo Dark.svg"} width={70} height={70} />
+      </Link>
       <FontAwesomeIcon icon={faBarsProgress} className='text-[#505050] font-[10px] text-4xl mt-[20px] p-[10px] cursor-pointer ease-in duration-200 hover:text-white hover:bg-[#212121] hover:text-[] rounded-xl'/>
     </div>
     <div className='flex justify-center mb-[20px]'>
