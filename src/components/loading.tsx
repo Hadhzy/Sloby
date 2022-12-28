eb78300bf0390d4ae6884eefacfcfcf22f746313
Coddo-Python <coddobusiness@gmail.com>
2022-12-28 19:11:01 +0400

    Add automatic loading animation

---
+++ src/components/loading.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center h-screen w-screen bg-dark-border">
+            <div className="animate-spin rounded-full h-64 w-64 border-b-4 border-white">
+                <Image alt="Sloby Logo" className={"rotate-45"} src={"/images/Sloby Logo Dark.svg"} width={150}
+                       height={150}/>
+            </div>
+        </div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,25 @@
 import '../utils/styles/globals.css'
 
-import type { AppProps } from 'next/app'
-import {useState} from "react";
+import type {AppProps} from 'next/app'
+import {Suspense, useState} from "react";
 import {createBrowserSupabaseClient} from "@supabase/auth-helpers-nextjs";
 import {SessionContextProvider} from "@supabase/auth-helpers-react";
-import { ProjectsContextProvider } from '../utils/contexts/ProjectsContext';
+import {ProjectsContextProvider} from '../utils/contexts/ProjectsContext';
+import Loading from "../components/loading";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
     const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
     return (
-    <ProjectsContextProvider>
-        <SessionContextProvider
-            supabaseClient={supabaseClient}
-            initialSession={pageProps.initialSession}
+        <ProjectsContextProvider>
+            <SessionContextProvider
+                supabaseClient={supabaseClient}
+                initialSession={pageProps.initialSession}
             >
-            <Component {...pageProps} />
-        </SessionContextProvider>
-    </ProjectsContextProvider>
+                <Suspense fallback={<Loading/>}>
+                    <Component {...pageProps} />
+                </Suspense>
+            </SessionContextProvider>
+        </ProjectsContextProvider>
     )
 }
