b8b9e272028ecb9f549acd3120a8073a22ea3410
Coddo-Python <coddobusiness@gmail.com>
2022-12-29 11:33:59 +0400

    Add star fade effect

--- src/components/LandingPage/star.tsx
+++ src/components/LandingPage/star.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import {useEffect, useMemo, useState} from "react";
+import {motion} from "framer-motion";
 
 export function Star() {
     const [transform, setTransform] = useState("translate(0)");
@@ -17,8 +18,12 @@ export function Star() {
     }, []);
 
     return (
-        <Image src="/images/Star.svg" className="w-2 absolute top-0 left-0 z-5"
-               style={{top: top, left: left, transform: transform}} alt="Star" width={20}
-               height={20}/>
+        <motion.div
+            initial={{opacity: 0}} animate={{opacity: 1,}} transition={{delay: 1, duration: 1.5, ease: "easeInOut"}}
+        >
+            <Image src="/images/Star.svg" className="w-2 absolute top-0 left-0 z-5"
+                   style={{top: top, left: left, transform: transform}} alt="Star" width={20}
+                   height={20}/>
+        </motion.div>
     )
 }
