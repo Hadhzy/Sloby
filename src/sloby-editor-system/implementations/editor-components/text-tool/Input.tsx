a6189258ae5222f053b22e9cfec8fac3be8a6948
gaborhadhazy <gaborhadhazy07@gmail.com>
2023-01-25 23:00:10 +0100

    Readd input styles

--- src/sloby-editor-system/implementations/editor-components/text-tool/Input.tsx
+++ src/sloby-editor-system/implementations/editor-components/text-tool/Input.tsx
@@ -2,5 +2,11 @@ import React, { useState } from 'react';
 import { BaseClassNames } from '../../../lib/grammar/BaseClassNames';
 
 export default function Input() {
-  return <input placeholder="type your text here..." type="text" />;
+  return (
+    <input
+      placeholder="type your text here..."
+      type="text"
+      className={`border border-blue-600 hover:decoration-2 duration-75  decoration-blue-400 hover:underline ${BaseClassNames.BASIC_DIV} bg-transparent`}
+    />
+  );
 }
