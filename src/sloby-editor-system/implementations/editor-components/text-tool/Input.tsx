7d7c9f711f5a30083eaf97feed2e9e4f3af9cd52
mohidenabdulkarim <mohidendj@gmail.com>
2023-02-01 11:08:28 +0300

    dragging first draft

--- src/sloby-editor-system/implementations/editor-components/text-tool/Input.tsx
+++ src/sloby-editor-system/implementations/editor-components/text-tool/Input.tsx
@@ -33,7 +33,11 @@ export default function Input({
   const [value, setValue] = useState('');
   const [optionsState, setOptionsState] = useState(false);
   const { inputs, setInputs } = useContext(InputsContext);
-  const inputRef = useRef<any>();
+
+  //for dragging
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const inputRef = useRef<HTMLInputElement>(null);
+
 
   const { handleChange } = useContext(InputsContext);
 
@@ -45,41 +49,41 @@ export default function Input({
     return setInputs(inputs.filter((item) => item.id !== e.currentTarget.id));
   };
 
-  const handleMouseDown = (e: any) => {
-    inputRef.current.style.transition = 'none';
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    const { left, top } = inputRef.current.getBoundingClientRect();
-    const dx = mouseX - left;
-    const dy = mouseY - top;
-    const handleMouseMove = (e: any) => {
-      const x = e.clientX - dx;
-      const y = e.clientY - dy;
-      const currentInput = inputs.find(
-        (item) => item.id === e.currentTarget.id
-      );
-      if (currentInput) {
-        currentInput.position.x = x;
-        currentInput.position.y = y;
-        setInputs([...inputs, currentInput]);
-      }
+  const handleMouseDown = (event: React.MouseEvent<HTMLInputElement>) => {
+    const input = inputRef.current;
+    if (!input) return;
+
+    const initialX = event.clientX - position.x;
+    const initialY = event.clientY - position.y;
+
+    const handleMouseMove = (moveEvent: MouseEvent) => {
+      console.log("moving");
+      setPosition({
+        x: moveEvent.clientX - initialX,
+        y: moveEvent.clientY - initialY,
+      });
     };
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', () => {
-      inputRef.current.style.transition = 'all 0.2s ease-out';
-      window.removeEventListener('mousemove', handleMouseMove);
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    document.addEventListener("mouseup", () => {
+      document.removeEventListener("mousemove", handleMouseMove);
     });
   };
 
   useEffect(() => {
-    console.log('Adding new an input');
-  }, [toolClicked]);
+    const input = inputRef.current;
+    if (!input) return;
 
-  useEffect(() => {
-    if (inputRef) {
-      inputRef.current.focus();
-    }
-  }, [toolClicked]);
+    input.style.left = `${position.x}px`;
+    input.style.top = `${position.y}px`;
+  }, [position]);
+
+  // useEffect(() => {
+  //   if (inputRef) {
+  //     inputRef.current.focus();
+  //   }
+  // }, [toolClicked]);
 
   // useEffect(() => {
   //   async function handleInput() {
@@ -104,52 +108,35 @@ export default function Input({
 
   return (
     <div
+      ref={inputRef}
       className={`max-w-fit relative group rounded overflow-hidden ease-in-out duration-150`}
     >
+
       <input
-        // onDragStart={(e) => {
-        // window.addEventListener('mouseover', (e) => {
-        // console.log("X: " + e.clientX + " | " + "Y: " + e.clientY);
-        // })
-        // }}
-        onDragEnd={(e) => {
-          // console.log("X: " + e.screenX + " | " + "Y: " + e.screenY);
-          const item = document.querySelector(
-            '.tool-drag-element'
-          ) as HTMLInputElement;
-          item.style.transform = `translate(${e.clientX}px,${e.clientY}px)`;
-          // console.log(window.getComputedStyle(item).getPropertyValue("transform"));
-          // const style = window.getComputedStyle(item);
-          // const matrix = new WebKitCSSMatrix(style.transform);
-          // console.log(matrix);
-        }}
-        ref={inputRef}
+        onMouseDown={handleMouseDown}
         onKeyDown={(e) => {
           if (e.key === 'Delete') {
             return handleDelete(e);
           }
         }}
-        draggable
         id={input.id}
         value={input.value}
         onChange={(e) => handleChange(e, index)}
         placeholder="type your text here..."
         type="text"
-        className={` mt-0 hover:cursor-pointer border-transparent  translate-x-0 translate-y-0 tool-drag-element duration-75 ${BaseClassNames.BASIC_DIV} bg-transparent`}
+        className={`mt-0 hover:cursor-pointer border-transparent  translate-x-0 translate-y-0 tool-drag-element duration-75 ${BaseClassNames.BASIC_DIV} bg-transparent`}
       />
       <button
         onClick={optionsToggle}
-        className={`absolute top-0 right-0 invisible group-hover:visible py-1 px-2 hover:scale-110  duration-150 ease-in-out ${
-          optionsState ? 'hidden' : 'visible'
-        } `}
+        className={`absolute top-0 right-0 invisible group-hover:visible py-1 px-2 hover:scale-110  duration-150 ease-in-out ${optionsState ? 'hidden' : 'visible'
+          } `}
       >
         <FontAwesomeIcon icon={faEllipsisVertical} className="text-lg" />
       </button>
 
       <div
-        className={`absolute top-0 right-0 ${
-          optionsState ? 'translate-y-0' : '-translate-y-6'
-        } transition-all`}
+        className={`absolute top-0 right-0 ${optionsState ? 'translate-y-0' : '-translate-y-6'
+          } transition-all`}
       >
         <button>
           <FontAwesomeIcon icon={faFaceSmile} className="p-1" />
