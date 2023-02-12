6fdb4788afbf5f7f9d341c1e0b14c510ff1cb3b8
gaborhadhazy <gaborhadhazy07@gmail.com>
2023-02-12 11:58:48 +0000

    Try to debug logging issue

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -19,10 +19,7 @@ export async function loggedIn(
 }
 
 export const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    `${process?.env?.SLOBY_URL}/editor/dashboard`;
+  let url = `${process?.env?.SLOBY_URL}/editor/dashboard`;
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
