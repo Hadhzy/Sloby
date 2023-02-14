85d546774b2e684c777c991556363106a00ee850
gaborhadhazy <gaborhadhazy07@gmail.com>
2023-02-14 14:36:17 +0000

    Get outta unnecesarry imports.

--- src/components/LandingPage/tabs.tsx
+++ src/components/LandingPage/tabs.tsx
@@ -1,28 +1,27 @@
-import Link from "next/link";
-import React from "react";
-import styled from "styled-components";
+import Link from 'next/link';
+import React from 'react';
 
 const tabsData = [
   {
-    title: "Quick Start",
-    value: "Quick Start",
-    link: "/quickstart"
+    title: 'Quick Start',
+    value: 'Quick Start',
+    link: '/quickstart',
   },
   {
-    title: "Documentation",
-    value: "Documentation",
-    link: "/documentation"
+    title: 'Documentation',
+    value: 'Documentation',
+    link: '/documentation',
   },
   {
-    title: "Templates",
-    value: "Templates",
-    link: "/templates"
+    title: 'Templates',
+    value: 'Templates',
+    link: '/templates',
   },
   {
-    title: "Blog",
-    value: "Blog",
-    link: "/blog"
-  }
+    title: 'Blog',
+    value: 'Blog',
+    link: '/blog',
+  },
 ];
 
 const Tabs = () => {
@@ -34,7 +33,7 @@ const Tabs = () => {
   const highlightRef = React.useRef<any>(null);
   const wrapperRef = React.useRef<any>(null);
 
-  const repositionHighlight = (e:any, tab:any) => {
+  const repositionHighlight = (e: any, tab: any) => {
     setTabBoundingBox(e.target.getBoundingClientRect());
     setWrapperBoundingBox(wrapperRef.current.getBoundingClientRect());
     setIsHoveredFromNull(!highlightedTab);
@@ -48,11 +47,11 @@ const Tabs = () => {
     opacity: 0,
     width: '',
     transform: '',
-    left: ''
-}
+    left: '',
+  };
 
   if (tabBoundingBox && wrapperBoundingBox) {
-    highlightStyles.transitionDuration = isHoveredFromNull ? "0ms" : "150ms";
+    highlightStyles.transitionDuration = isHoveredFromNull ? '0ms' : '150ms';
     highlightStyles.opacity = highlightedTab ? 1 : 0;
     highlightStyles.width = `${tabBoundingBox.width}px`;
     highlightStyles.transform = `translate(${
@@ -62,9 +61,18 @@ const Tabs = () => {
 
   return (
     <div ref={wrapperRef} onMouseLeave={resetHighlight} className="relative">
-      <div ref={highlightRef} style={highlightStyles} className="nav-highlight"></div>
+      <div
+        ref={highlightRef}
+        style={highlightStyles}
+        className="nav-highlight"
+      ></div>
       {tabsData.map((tab) => (
-        <Link href={tab.link} key={tab.value} onMouseOver={(ev) => repositionHighlight(ev, tab)} className="nav-button">
+        <Link
+          href={tab.link}
+          key={tab.value}
+          onMouseOver={(ev) => repositionHighlight(ev, tab)}
+          className="nav-button"
+        >
           {tab.title}
         </Link>
       ))}
