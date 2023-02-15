719c3e1e1dee99d3ca28fbfe5b7b0bf868f18faf
gaborhadhazy <gaborhadhazy07@gmail.com>
2023-02-15 18:41:10 +0000

    Websocket integration.

--- src/pages/api/websocket.ts
+++ src/pages/api/websocket.ts
@@ -1,32 +1,35 @@
-import WebSocket from 'ws';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+import { WebSocketServer } from 'ws';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Create a new WebSocket server
-  const wss = new WebSocket.Server({ noServer: true });
+const SocketHandler = async (req: any, res: any) => {
+  if (res.socket.server.wss) {
+    console.log('Socket is already running');
+  } else {
+    console.log('Socket is initializing');
+    const server = res.socket.server;
+    const wss = new WebSocketServer({ noServer: true });
+    res.socket.server.wss = wss;
 
-  // Handle WebSocket connections
-  wss.on('connection', (socket: WebSocket) => {
-    console.log('Client connected');
+    server.on('upgrade', (req: any, socket: any, head: any) => {
+      console.log('upgrade', req.url);
 
-    socket.on('message', (message: string) => {
-      console.log(`Received message: ${message}`);
-
-      // Echo the message back to the client
-      socket.send(`You said: ${message}`);
+      if (!req.url.includes('/_next/webpack-hmr')) {
+        wss.handleUpgrade(req, socket, head, (ws) => {
+          wss.emit('connection', ws, req);
+        });
+      }
     });
 
-    // Send a greeting message to the client
-    socket.send('Hello, client!');
-  });
+    wss.on('connection', (ws) => {
+      console.log('connection', ws);
+      ws.on('message', (data) => {
+        console.log('received: %s', data);
+      });
 
-  // Upgrade the HTTP request to a WebSocket connection
-  req.socket.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, (socket) => {
-      wss.emit('connection', socket, request);
+      ws.send('something');
     });
-  });
+  }
+  res.end();
+};
 
-  // Send an HTTP response to indicate that the upgrade was successful
-  res.status(200).end();
-}
+export default SocketHandler;
