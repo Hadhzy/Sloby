c61c1cb2f82bbde3f62d3415c15e27643ea3caff
gaborhadhazy <gaborhadhazy07@gmail.com>
2023-02-25 13:07:34 +0000

    Preview site problem.

--- src/pages/editor/preview/[id]/index.tsx
+++ src/pages/editor/preview/[id]/index.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
-import { ProjectServices } from '../../../../api/project.api';
+import React, { useEffect, useState } from 'react';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/router';
-import { TSlobyProject } from '../../../../utils/types';
 import ElementModifier from '../../../../sloby-editor-system/implementations/html_rendering/ElementModifier';
+import { ProjectServices } from '../../../../api/project.api';
 import ReactHtmlParser from 'react-html-parser';
+import { TSlobyProject } from '../../../../utils/types';
 
 export default function PreViewSite() {
   const [site, setSite] = React.useState<Array<TSlobyProject>>([]);
   const [sourceCode, setSourceCode] = React.useState<string>('');
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const supabase = useSupabaseClient();
   const router = useRouter();
   const transformator = new ElementModifier();
@@ -18,28 +19,41 @@ export default function PreViewSite() {
     ignoreTags: ['script', 'style'],
   };
 
-  async function getSite() {
-    const res = new ProjectServices(supabase);
-    const html = await res.getProjectsSource(router.query.id as string);
-
-    console.log(html.data?.interface_source);
-    if (html.data?.interface_source) {
-      setSite(html.data?.interface_source);
-
-      // site.map((item: any) => {
-      //   const html = transformator.inputToParagraph(item);
-      //   setSourceCode(html);
-      // });
+  useEffect(() => {
+    async function getSite() {
+      if (site && site.length > 0) {
+        site.map((item: any) => {
+          const html_sourceCode = transformator.inputToParagraph(item);
+          setSourceCode((prev: string) => prev + html_sourceCode);
+        });
+      }
     }
-  }
+
+    async function getDbStructure() {
+      const res = new ProjectServices(supabase);
+      let html = await res.getProjectsSource(router.query.id as string);
+      if (html) {
+        setSite(html.data?.interface_source);
+      }
+      setIsLoading(false);
+    }
+
+    setIsLoading(true);
+    getDbStructure();
+    getSite();
+  }, [router.query.id]);
 
   useEffect(() => {
-    getSite();
-  }, []);
+    if (sourceCode !== '') {
+      setIsLoading(false);
+    }
+  }, [sourceCode]);
 
   return (
     <div className="bg-interface-bg w-screen h-screen">
-      {sourceCode !== '' ? (
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : sourceCode !== '' ? (
         <div> {ReactHtmlParser(sourceCode, options)}</div>
       ) : (
         ''
